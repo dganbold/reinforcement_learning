a5020e9270aac5368cf213abb8ad5cfb18e15d42
Ganbold <ganbold_0324@yahoo.com>
2017-10-28 18:02:50 +0900

    Q-network trained on CartPole-v0

---
+++ NeuralQLearning/cartpole_test.py
@@ -0,0 +1,80 @@
+#
+#
+import gym
+from NeuralQLearner import *
+#
+#
+if __name__ == "__main__":
+    # ----------------------------------------
+    # Define parameters for greedy policy
+    epsilon = 0.5   # exploration
+    epsilon_floor = 0.1
+    exploration_decay = 0.998
+    # Define parameters for Q-learning
+    alpha = 0.2
+    gamma = 0.98
+    epoch = 10
+    max_steps = 200
+    max_memory = max_steps*10
+    batch_size = int(32)
+    # ----------------------------------------
+    # Actions
+    # Type: Discrete(2)
+    # Num | Observation
+    # 0   | Push cart to the left
+    # 1   | Push cart to the right
+    n_action = 2
+    actions = np.array([0, 1])
+    # ----------------------------------------
+    # Observation
+    # Type: Box(4)
+    # Num | Observation   | Min    | Max
+    # 0   | Cart Position | -2.4   | 2.4
+    # 1   | Cart Velocity | -Inf   | Inf
+    # 2   | Pole Angle    | -41.8  | 41.8
+    # 3   | Pole Velocity | -Inf   | Inf
+    n_input = 4
+    observation = []
+    # ----------------------------------------
+    # Define environment/game
+    env_name = 'CartPole-v0'
+    env = gym.make(env_name)
+    # ----------------------------------------
+    # Initialize Neural Q-Learn object
+    AI = NeuralQLearner(n_input, actions, batch_size, epsilon, alpha, gamma)
+    # Load pre-trained model
+    AI.importNetwork('models/%s_Q_network_epoch_1000' % (env_name))
+    #AI.plotQ()
+    # ----------------------------------------
+    # Train
+    for e in range(epoch):
+        # Get initial input
+        observation = env.reset()
+        observation_init = observation
+
+        # Training for single episode
+        step = 0
+        total_reward = 0
+        game_over = False
+        while (not game_over):
+            observation_capture = observation
+            env.render()
+
+            # Greedy policy
+            action = AI.greedy(observation)
+
+            # Apply action, get rewards and new state
+            observation, reward, game_over, info = env.step(action)
+            #
+            step += 1
+            total_reward += reward
+        # End of the single episode testing
+        print('#TEST Episode:%2i, Reward:%7.3f, Steps:%3i' % (e, total_reward, step))
+        # Plot
+        #
+    # ----------------------------------------
+    print("Done!.")
+    # Close environment
+    env.close()
+    # Some delay
+    raw_input('Press enter to terminate:')
