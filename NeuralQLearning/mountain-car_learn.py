dd981da00474599727cf774a0ff718e220e37e76
Your Name <you@example.com>
2017-11-08 18:12:31 +0900

    Added reward ploting part.

--- NeuralQLearning/mountain-car_learn.py
+++ NeuralQLearning/mountain-car_learn.py
@@ -2,21 +2,31 @@
 #
 import gym
 from NeuralQLearner import *
+from PlotWrapper import *
 #
 #
 if __name__ == "__main__":
     # ----------------------------------------
-    # Define parameters for greedy policy
+    # Define parameters for e-Greedy policy
     epsilon = 0.5   # exploration
-    epsilon_floor = 0.05
+    epsilon_floor = 0.1
     exploration_decay = 0.998
     # Define parameters for Q-learning
     alpha = 0.2
     gamma = 0.98
     epoch = 1000
-    max_steps = 200
-    max_memory = max_steps*10
-    batch_size = int(32)
+    max_steps = 500
+    # Define parameters for Q-network
+    batch_size = 32
+    hidden_neurons = 50
+    update_target = 32
+    max_memory = 10000
+    #
+    render = False
+    # ----------------------------------------
+    # Define environment/game
+    env_name = 'MountainCar-v0'
+    env = gym.make(env_name)
     # ----------------------------------------
     # Actions
     # Type: Discrete(3)
@@ -24,7 +34,7 @@ if __name__ == "__main__":
     # 0   | push_left
     # 1   | no_push
     # 2   | push_right
-    n_action = 3
+    n_action = env.action_space.n
     actions = np.array([0, 1, 2])
     # ----------------------------------------
     # Observation
@@ -32,32 +42,37 @@ if __name__ == "__main__":
     # Num | Observation | Min   | Max
     # 0   | position    | -1.2  | 0.6
     # 1   | velocity    | -0.07 | 0.07
-    n_input = 2
+    n_input = env.observation_space.shape[0]
     observation = []
     # ----------------------------------------
-    # Define environment/game
-    env_name = 'MountainCar-v0'
-    env = gym.make(env_name)
+    # Initialize Plot object
+    R_plot = PlotTimeSeries(x_lim=[0, 10], y_lim=[-max_steps-10, 0], size=(6.8, 5.0))
+    R_plot.create([list(), list()], 'Episode', 'Total Reward', 'Neural Q-Learning: ' + env_name)
+    #
+    titles = "Actions " + u"\u25C0" + ":push_left/" + u"\u25AA" + ":no_push/" + u"\u25B6" + ":push_right"
+    Q_plot = Plot2D(x_lim=[-1.2, 0.6], y_lim=[-0.07, 0.07], x_n=27, y_n=27, size=(5.8, 5.0))
+    S_mesh = Q_plot.getMeshGrid()
     # ----------------------------------------
-    # Initialize Neural Q-Learn object
-    AI = NeuralQLearner(n_input, actions, batch_size, epsilon, alpha, gamma)
-    AI.plotQ()
+    # Initialize Neural Q-Learner object
+    AI = NeuralQLearner(n_input, actions, hidden_neurons, batch_size, update_target, epsilon, alpha, gamma)
+    Q_mesh = AI.evaluate_Q_target(S_mesh)
+    # Plot
+    Q_plot.create(Q_mesh, 'Position', 'Velocity', titles)
     # Initialize experience replay object
-    exp = Experience(max_memory)
+    experienceMemory = Experience(max_memory)
     # ----------------------------------------
     # Train
     for e in range(epoch):
         # Get initial input
         observation = env.reset()
-        observation_init = observation
 
         # Training for single episode
         step = 0
         total_reward = 0
         game_over = False
-        while (not game_over):
+        while not game_over:
             observation_capture = observation
-            #env.render()
+            if render: env.render()
 
             # Epsilon-Greedy policy
             action = AI.eGreedy(observation)
@@ -68,7 +83,7 @@ if __name__ == "__main__":
                 reward = 1
                 game_over = True
             else:
-                reward += 5.*np.abs(observation[1])
+                reward += 4.*np.abs(observation[1])
 
             step += 1
             if step >= max_steps and done:
@@ -76,10 +91,10 @@ if __name__ == "__main__":
 
             # Store experience
             # input[i] = [[state_t, action_t, reward_t, state_t+1], game_over?]
-            exp.memorize([observation_capture, action, reward, observation], game_over)
+            experienceMemory.memorize([observation_capture, action, reward, observation], game_over)
 
             # Recall and replay experience
-            miniBatch = exp.recall(batch_size)
+            miniBatch = experienceMemory.recall(batch_size)
             # Refinement of model
             if len(miniBatch) == batch_size:
                 AI.train_Q_network(miniBatch)
@@ -91,10 +106,12 @@ if __name__ == "__main__":
         AI.epsilon *= exploration_decay
         AI.epsilon = max(epsilon_floor, AI.epsilon)
         # Plot
-        AI.plotQupdate()
+        R_plot.append([e, total_reward])
+        Q_mesh = AI.evaluate_Q_target(S_mesh)
+        Q_plot.update(Q_mesh)
         #
     # ----------------------------------------
-    # Export trained Neural-Net
+    # Export trained Q-Network
     AI.exportNetwork('models/%s_Q_network_epoch_%d' % (env_name, epoch))
     # ----------------------------------------
     print("Done!.")
--- NeuralQLearning/mountain-car_test.py
+++ NeuralQLearning/mountain-car_test.py
@@ -2,19 +2,31 @@
 #
 import gym
 from NeuralQLearner import *
+from PlotWrapper import *
 #
 #
 if __name__ == "__main__":
     # ----------------------------------------
     # Define parameters for e-Greedy policy
-    epsilon = 1.0   # exploration
+    epsilon = 0.5   # exploration
     epsilon_floor = 0.1
-    exploration_decay = 0.995
+    exploration_decay = 0.998
     # Define parameters for Q-learning
     alpha = 0.2
-    gamma = 0.97
+    gamma = 0.98
     epoch = 10
-    batch_size = int(32)
+    max_steps = 500
+    # Define parameters for Q-network
+    batch_size = 128
+    hidden_neurons = 50
+    update_target = 32
+    max_memory = 10000
+    #
+    render = True
+    # ----------------------------------------
+    # Define environment/game
+    env_name = 'MountainCar-v0'
+    env = gym.make(env_name)
     # ----------------------------------------
     # Actions
     # Type: Discrete(3)
@@ -22,36 +34,37 @@ if __name__ == "__main__":
     # 0   | push_left
     # 1   | no_push
     # 2   | push_right
-    n_action = 3
-    actions = [0, 1, 2]
+    n_action = env.action_space.n
+    actions = np.array([0, 1, 2])
     # ----------------------------------------
     # Observation
     # Type: Box(2)
     # Num | Observation | Min   | Max
     # 0   | position    | -1.2  | 0.6
     # 1   | velocity    | -0.07 | 0.07
-    n_input = 2
+    n_input = env.observation_space.shape[0]
     observation = []
     # ----------------------------------------
-    # Define environment/game
-    env_name = 'MountainCar-v0'
-    env = gym.make(env_name)
+    # Initialize Plot object
+    titles = "Actions " + u"\u25C0" + ":push_left/" + u"\u25AA" + ":no_push/" + u"\u25B6" + ":push_right"
+    Q_plot = Plot2D(x_lim=[-1.2, 0.6], y_lim=[-0.07, 0.07], x_n=27, y_n=27, size=(5.8, 5.0))
+    S_mesh = Q_plot.getMeshGrid()
     # ----------------------------------------
-    # Initialize QLearn object
-    AI = NeuralQLearner(n_input, actions, batch_size, epsilon, alpha, gamma)
+    # Initialize Neural Q-Learner object
+    AI = NeuralQLearner(n_input, actions, hidden_neurons, batch_size, update_target, epsilon, alpha, gamma)
     # Load pre-trained model
     AI.importNetwork('models/%s_Q_network_epoch_1000' % (env_name))
-    # Plot Eval-Q
-    AI.plotQ()
-    AI.plotQaction()
+    # Evaluate Q-network and plot
+    Q_mesh = AI.evaluate_Q_target(S_mesh)
+    Q_plot.create(Q_mesh, 'Position', 'Velocity', titles)
     # ----------------------------------------
     # Test
+    observation = env.reset()
     env.render()
     raw_input('Press enter to start:')
     for e in range(epoch):
         # Clear trajectory
-        AI.clearTrajectory()
-        plt.pause(0.001)
+        Q_plot.deletePoints()
 
         # Get initial input
         observation = env.reset()
@@ -59,9 +72,9 @@ if __name__ == "__main__":
         step = 0
         total_reward = 0
         game_over = False
-        while (not game_over):
+        while not game_over:
             observation_capture = observation
-            env.render()
+            if render: env.render()
 
             # Greedy policy
             action = AI.greedy(observation)
@@ -70,7 +83,7 @@ if __name__ == "__main__":
             observation, reward, game_over, info = env.step(action)
 
             # Plot trajectory
-            AI.plotTrajectory(observation_capture, action)
+            Q_plot.appendPoint(observation_capture)
             #
             step += 1
             total_reward += reward
